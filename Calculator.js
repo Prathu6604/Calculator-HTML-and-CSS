3a6f40e8d94cd999d4d69789c74accfc574abf25
Prathamesh Kumbhar <154219641+Prathu6604@users.noreply.github.com>
2024-03-07 21:53:14 +0530

    Website of Calculator HTML and CSS

---
+++ Calculator.js
@@ -0,0 +1,21 @@
+let displayValue="";
+
+function show(value){
+    displayValue +=value;
+    document.getElementById("display").value=displayValue;
+}
+
+function clearDisplay(){
+    displayValue="";
+    document.getElementById("display").value=displayValue;
+}
+
+function calculate(){
+    try{
+        const result=eval(displayValue);
+        document.getElementById("display").value=result;
+        displayValue=result;
+    }catch(error){
+        document.getElementById("display").value="Error";
+    }
+}
